98da3625dd788a48ec2a24edf1d6b4018c85ab1f
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-02 11:35:35 +0200

    pendiente terminar

--- modulo-2-leccion-09-ejercicio-2.9.4/js/main.js
+++ modulo-2-leccion-09-ejercicio-2.9.4/js/main.js
@@ -1,25 +1,49 @@
 'use strict';
 
-const main = document.querySelector('.js-main');
-
+const ul = document.querySelector('.js-ul');
+let input = '';
 const tasks = [
   { name: 'Recoger setas en el campo', completed: true },
   { name: 'Comprar pilas', completed: true },
   { name: 'Poner una lavadora de blancos', completed: true },
   {
     name: 'Aprender cómo se realizan las peticiones al servidor en JavaScript',
-    completed: false
-  }
+    completed: false,
+  },
 ];
 
-for( const data of tasks ) {
-  if( data.completed ) {
-    const html = `<li class="completed">${data.name}</li>`;
-    main.innerHTML += html;
+function forObject() {
+  for (const data of tasks) {
+    if (data.completed) {
+      input = '<input checked type="checkbox" class="inputClass"/>';
+      const html = `<li class="completed">${data.name}</li>${input}`;
+      ul.innerHTML += html;
+    } else {
+      input = '<input type="checkbox" class="inputClass"/>';
+      const html = `<li class="">${data.name}</li>${input}`;
+      ul.innerHTML += html;
+    }
   }
-  else {
-    const html = `<li>${data.name}</li>`;
-    main.innerHTML += html;
-  }
- 
+  listenerEv();
 }
+function listenerEv() {
+  const checkboxList = document.querySelectorAll('.inputClass');
+  console.log(checkboxList); //array checkbox
+  for (const checkbox of checkboxList) {
+    checkbox.addEventListener('click', inputCheck);
+  }
+}
+function inputCheck(event) {
+  debugger;
+  if (input.checked === true) {
+    input.checked = true;
+    console.log('Checked');
+  } else {
+    // input.checked = false;
+    console.log('No checked');
+    tasks[event].completed = false;
+  }
+  console.log(tasks);
+}
+
+forObject();
