92786f8fd971d569960e148624858479c8c76754
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-07 11:30:32 +0200

    add new exercice

--- module-2-lesson-12-exercise-6/js/main.js
+++ module-2-lesson-12-exercise-6/js/main.js
@@ -1,17 +1,28 @@
 'use strict';
-const list = document.querySelector('.js-list');
-const input = document.querySelector('.js-input');
-const btn = document.querySelector('.js-btn');
+const body = document.querySelector('body');
+//const main = document.querySelector('.js_main');
+const ul = document.querySelector('.js_ul');
+const btn = document.querySelector('.js_btn');
+body.style = 'background-color:black; height:100vh; color:white;'; // solo usar si se debe evaluar desde JS - sino son buenas prácticas escribirlo en CSS
 
-function getCharacter(ev) {
-  ev.preventDefault();
-
-  let character = input.value;
-  fetch(`https://swapi.dev/api/people/?search=${character}`)
-    .then((response) => response.json())
-    .then((results) => {
-      console.log(results);
-      list.innerHTML += `<li>Personaje: ${results[name]}</li>`;
-    });
+function hendelClick100(ev) {
+  let newEl = document.createElement('li');
+  let newText = document.createTextNode('Hola, como estas?');
+  debugger;
+  newEl.appendChild(newText);
+  ul.appendChild(newEl);
+  newEl.removeChild(newText); //Eliminar texto! :)
+  newEl.remove(); //Elimina li
+  for (let index = 0; index < 100; index++) {
+    newText = document.createTextNode(
+      'He aprendido bien cómo funcionan los bucles'
+    );
+    newEl = document.createElement('li');
+    newEl.appendChild(newText);
+    ul.appendChild(newEl);
+  }
+  console.log(document.documentElement.parentNode); // busca de DOM - si no hay elemento padre te da el HTML entero.
+  console.log(document.documentElement.parentElement); // busca de HTML - si no es un elemento de DOM, te va a devolver null
 }
-btn.addEventListener('click', getCharacter);
+
+btn.addEventListener('click', hendelClick100);
