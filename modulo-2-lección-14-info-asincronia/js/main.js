641d2a5395a3c059aa5e6d44d7a5b13c18a7824e
JuditAldeguer <jud.vicens@hotmail.es>
2021-09-09 13:38:45 +0200

    ex update

--- modulo-2-lección-14-info-asincronia/js/main.js
+++ modulo-2-lección-14-info-asincronia/js/main.js
@@ -29,23 +29,80 @@
 // };
 // temp = setInterval(incrementAndShowCounter, 1000);
 
-//clearTimeout()
-let myVar;
+//Ejemplo vario --------------------
 let counter = 0;
-
+let resta = 10;
+const time = document.querySelector('.time');
 function secondCount() {
   counter++;
-  console.log(counter);
-
-  return counter;
+  time.innerHTML = counter;
 }
-setInterval(myFunction, 1000); // si no podemos parámetro tiempo, se ejecuta inmediatamente
+setInterval(secondCount, 1000); // si no podemos parámetro tiempo, se ejecuta inmediatamente
+let endSetInterval;
 function myFunction() {
-  let resta = 10 - counter;
-  myVar = alert(`CUIDADO! Este texto desaparecerá en ${resta} segundos`); //no se actualiza
-  if (counter === 7) {
-    clearTimeout(endSetTimeout, 7000);
+  resta -= 1;
+  const msg = document.querySelector('.msg');
+  msg.innerHTML = `CUIDADO! Este texto desaparecerá en ${resta} segundos`;
+
+  if (endSetInterval === undefined) {
+    endSetInterval = setInterval(myFunction, 1000);
+  }
+
+  if (resta === 0) {
+    clearInterval(endSetInterval);
+    msg.innerHTML = '';
+    btn.innerHTML = 'Desapareció!';
   }
 }
-const endSetTimeout = setTimeout(myFunction, 3000);
-//-----------------------------da error/////////////////////
+setTimeout(myFunction, 3000);
+
+const btn = document.querySelector('.js_button');
+function resetBtn() {
+  btn.classList.remove('hidden');
+}
+btn.addEventListener('click', resetBtn);
+const resetBtnSetTime = setTimeout(resetBtn, 4000);
+
+//clearTimeout()
+//Es muy poco usado
+let counter2 = 0;
+let control;
+const time2 = document.querySelector('.time2');
+function secondCount2() {
+  if (counter2 < 6) {
+    counter2++;
+    time2.innerHTML = counter2;
+  } else if (counter2 === 6) {
+    counter2 = 0;
+    time2.innerHTML = counter2;
+  }
+  if (control === undefined) {
+    control = setInterval(secondCount2, 1000);
+  }
+}
+var myVar;
+const btnTryIt = document.querySelector('.js_btnTryIt');
+const btnStopIt = document.querySelector('.js_btnStopIt');
+function showAlert() {
+  counter2 = 0;
+  time2.innerHTML = counter2;
+  if (myVar === undefined) {
+    myVar = setTimeout(function () {
+      alert('Alert shown');
+    }, 5000);
+    secondCount2();
+    myVar = undefined;
+  } else {
+    time2.innerHTML = 0;
+    myStopFunction();
+  }
+}
+function myStopFunction() {
+  clearTimeout(myVar);
+  clearInterval(control);
+  control = undefined;
+  counter2 = 0;
+  time2.innerHTML = counter2;
+}
+btnTryIt.addEventListener('click', showAlert);
+btnStopIt.addEventListener('click', myStopFunction);
